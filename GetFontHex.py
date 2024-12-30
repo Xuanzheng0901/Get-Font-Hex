56ed41db819852ce6f050cd282e636c446d3b1df
Xuanzheng0901 <szx1691196289@gmail.com>
2024-12-30 22:25:56 +0800

    增加通过传入参数切换字体功能

--- GetFontHex.py
+++ GetFontHex.py
@@ -2,15 +2,20 @@ from PIL import Image, ImageFont, ImageDraw
 import sys
 
 
-def GetData(a):
+def GetData(a, b):
     image = Image.new("1", [16, 16], color=0)  # 创建黑底图片
     draw_obj = ImageDraw.Draw(image)  # 创建一个draw对象
-    font = ImageFont.truetype(font_path, 16, encoding="unic")
+    if b == 1:
+        font = ImageFont.truetype(font_path, 16, encoding="unic")
+    else:
+        font = ImageFont.truetype('%WINDIR%\\Fonts\\STXINWEI.TTF', encoding="unic")
+
     for char in a:
         for i in range(16):
             for j in range(16):
                 draw_obj.point((i, j), 0)  # 初始化图像为黑色
         draw_obj.text((0, 0), char, fill=255, font=font)  # 在图片上覆盖白色字体
+        image.save("3.bmp")
         data = [0] * 32
         fstring = [""] * 32
 
@@ -32,13 +37,13 @@ except:
     string = str(input())
     argv_flag = 0
 
-GetData(string)
+GetData(string, 1)
 if argv_flag:
     exit(0)  # 如果命令行传入参数则处理完后结束
 
 while True:
     try:
         string = str(input())
-        GetData(string)
+        GetData(string, 2)
     except KeyboardInterrupt:
         exit(0)
